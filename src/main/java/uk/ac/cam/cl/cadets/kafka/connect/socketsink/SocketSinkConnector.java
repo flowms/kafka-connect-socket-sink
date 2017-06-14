bc7fc113c63769f80cc85b8c084f52ea8a28d75d
Graeme Jenkinson <gcj21@cl.cam.ac.uk>
2017-06-14 13:37:10 +0000

    Baseline working verion of the socket-sink connector

---
+++ src/main/java/uk/ac/cam/cl/cadets/kafka/connect/socketsink/SocketSinkConnector.java
@@ -0,0 +1,85 @@
+/*-
+ * Copyright (c) 2017 (Graeme Jenkinson)
+ * All rights reserved.
+ *
+ * This software was developed by BAE Systems, the University of Cambridge
+ * Computer Laboratory, and Memorial University under DARPA/AFRL contract
+ * FA8650-15-C-7558 ("CADETS"), as part of the DARPA Transparent Computing
+ * (TC) research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+package uk.ac.cam.cl.cadets.kafka.connect.socketsink;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.apache.kafka.common.config.Config;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.sink.SinkConnector;
+import org.apache.kafka.connect.connector.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final public class SocketSinkConnector extends SinkConnector {
+
+    private static final Logger LOGGER =
+        LoggerFactory.getLogger(SocketSinkConnector.class);
+
+    private Map<String, String> configProps;
+
+    @Override
+    public ConfigDef config() {
+        return SocketSinkConfig.CONFIG_DEF;
+    }
+
+    @Override
+    public void start(Map<String, String> props) {
+        configProps = props;
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public Class<? extends Task> taskClass() {
+        return SocketSinkTask.class;
+    }
+
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        LOGGER.info("Setting task configurations for {} workers", maxTasks);
+        final List<Map<String, String>> configs = new ArrayList<>(maxTasks);
+        for (int i = 0; i < maxTasks; i++) {
+            configs.add(configProps);
+        }
+        return configs;
+    }
+
+    @Override
+    public String version() {
+        return Version.getVersion();
+    }
+}
